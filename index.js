0c59fa6ea087c25dd94904eb1fcafe9604d5f6ca
ansteh <stehle.andre@googlemail.com>
2016-05-20 23:32:20 +0200

    init

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 'use strict';
-const _   = require('lodash');
-const parse = require('url').parse;
+const _     = require('lodash');
+const parseUrl = require('url').parse;
 
-const parsePath = (url, pattern) => {
-  let pathname = parse(url).pathname;
-  let fields = _.split(pattern, "/");
-  if(_.get(fields, '0') === '') {
-    fields.shift();
-  }
-  let values = _.split(pathname, "/");
-  if(_.get(values, '0') === '') {
-    values.shift();
-  }
-  let collector = {};
-  _.forEach(fields, (field, index) => {
-    collector[index.toString()] = values[index];
-    if(_.startsWith(field, ':')) collector[_.trimStart(field, ':')] = values[index];
+//let pathname = parseUrl(urlStr).pathname;
+
+const parse = (pattern) => {
+  return _.map(_.split(pattern, '/'), (value, key) => {
+    if(_.startsWith(value, ':')){
+      return { key: _.trim(value, ':') };
+    }
+    return { value: value };
   });
-  return collector;
 };
 
-module.exports = {
-  parse: {
-    path: parsePath
+const params = (pattern) => {
+  return _.chain(parse(pattern)).filter('key').map('key').value();
+};
+
+const builPath = _.curry((pattern, provider) => {
+  let parsed = parse(pattern);
+  if(_.get(parsed, '0.value') === '') parsed.shift();
+
+  return _.reduce(parsed, (url, instance) => {
+    let dir = instance.value;
+    if(_.isUndefined(dir)) {
+      dir = provider[instance.key];
+    }
+    return `${url}/${dir}`;
+  }, '');
+});
+
+const buildUrl = _.curry((pattern, provider) => {
+  let url = parseUrl(pattern);
+  let prefix = '';
+  if(url.hostname){
+    prefix = `${url.protocol}//${url.hostname}`;
   }
+  return `${prefix}${builPath(url.pathname, provider)}`;
+});
+
+module.exports = {
+  parse: parse,
+  params: params,
+  builPath: builPath,
+  buildUrl: buildUrl
 };
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 'use strict';
 const shape = require('./index.js');
+const assert = require('assert');
 
-let parsed = shape.parse.path('/one/two/three', '//:mid/');
-console.log(parsed);
+let patternExmp = '/test/:mid/2/:a';
+assert.deepEqual(shape.parse(patternExmp), [{ value: '' }, { value: 'test' }, { key: 'mid' }, { value: '2' },{ key: 'a' } ]);
+assert.deepEqual(shape.params(patternExmp), ['mid', 'a']);
+assert.deepEqual(shape.buildUrl(patternExmp, { mid: 'mid', a: 123 }), '/test/mid/2/123');
+assert.deepEqual(shape.buildUrl('http://test.com/test/:mid/2/:a', { mid: 'mid', a: 123 }), 'http://test.com/test/mid/2/123');
